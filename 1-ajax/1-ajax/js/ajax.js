56521eac82c14a8a5f8d4d2df50df2c7d38bd823
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-25 13:20:03 +0200

    cf

--- 1-ajax/1-ajax/js/ajax.js
+++ 1-ajax/1-ajax/js/ajax.js
@@ -1,2 +1,21 @@
 'use strict';
 
+function pobierzDane() {
+    var httpReq = new XMLHttpRequest();
+    
+    httpReq.open("GET","http://echo.jsontest.com/userID/108/userName/Akademia108/userURL/akademia108.pl", true);
+    
+    httpReq.onreadystatechange = function () {
+        console.log(httpReq.readyState);
+        
+        if (httpReq.readyState == 4) {
+            console.log(httpReq.status);
+            
+            if(httpReq.status >= 200 && httpReq.status < 300 || httpReq.status == 304 || navigator.userAgent.indexOf("Safari") >= 0 && typeof httpReq.status == "undefined"){
+                httpReq = null;
+            }
+        }
+    }
+    httpReq.send();
+}
+pobierzDane();
