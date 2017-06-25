492698d476881e981fcb67f2c19b62b960f9d1a0
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-25 13:26:42 +0200

    json parse

--- 1-ajax/1-ajax/js/ajax.js
+++ 1-ajax/1-ajax/js/ajax.js
@@ -2,16 +2,22 @@
 
 function pobierzDane() {
     var httpReq = new XMLHttpRequest();
-    
-    httpReq.open("GET","http://echo.jsontest.com/userID/108/userName/Akademia108/userURL/akademia108.pl", true);
-    
+
+    httpReq.open("GET", "http://echo.jsontest.com/userID/108/userName/Akademia108/userURL/akademia108.pl", true);
+
     httpReq.onreadystatechange = function () {
         console.log(httpReq.readyState);
-        
+
         if (httpReq.readyState == 4) {
             console.log(httpReq.status);
-            
-            if(httpReq.status >= 200 && httpReq.status < 300 || httpReq.status == 304 || navigator.userAgent.indexOf("Safari") >= 0 && typeof httpReq.status == "undefined"){
+
+            if (httpReq.status >= 200 && httpReq.status < 300 || httpReq.status == 304 || navigator.userAgent.indexOf("Safari") >= 0 && typeof httpReq.status == "undefined") {
+                
+                console.log(typeof httpReq.responseText);
+                var returnData = JSON.parse(httpReq.responseText);
+                
+                console.log(returnData);
+                
                 httpReq = null;
             }
         }
